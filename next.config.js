ce442be6f6cfe76d2cddde2d8879ca0b469cd237
Scott Motte <mot@mot.la>
2022-10-17 16:34:29 -0700

    Make ready with dotenv-vault

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const result = require('dotenv-vault-core').config()
+console.log(result)
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: result.parsed
 }
 
 module.exports = nextConfig
